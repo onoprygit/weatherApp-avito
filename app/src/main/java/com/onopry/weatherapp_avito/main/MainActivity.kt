9a15ffcc3719fb4da3ad7ab57bc3dc549027b4d4
Andrey Onopriychuk <andromeda2516@gmail.com>
2022-11-14 19:09:14 +0700

    add handle location permission

--- app/src/main/java/com/onopry/weatherapp_avito/main/MainActivity.kt
+++ app/src/main/java/com/onopry/weatherapp_avito/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.onopry.weatherapp_avito.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.onopry.weatherapp_avito.R
+import com.onopry.weatherapp_avito.presentation.screens.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/onopry/weatherapp_avito/presentation/screens/HomeFragment.kt
+++ app/src/main/java/com/onopry/weatherapp_avito/presentation/screens/HomeFragment.kt
@@ -1,13 +1,91 @@
 package com.onopry.weatherapp_avito.presentation.screens
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.provider.Settings
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
 import com.onopry.weatherapp_avito.R
+import com.onopry.weatherapp_avito.databinding.FragmentHomeBinding
+import com.onopry.weatherapp_avito.utils.shortToast
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        binding.textview.text = "asdasasdasd"
+        checkAppPermissions()
+    }
+
+    private fun checkAppPermissions() {
+        if (checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) {
+            shortToast("Разрешение есть")
+        } else {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                COERCE_LOCATION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == COERCE_LOCATION_REQUEST_CODE) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                shortToast("Разрешенгие получено")
+            }
+            else{
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)){
+                    askUserForOpeningAppSettings()
+                }
+            }
+        }
+    }
+
+    private fun askUserForOpeningAppSettings(){
+        val appSettingIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", requireContext().packageName, null)
+        )
+
+        if (requireContext().packageManager.resolveActivity(appSettingIntent, PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            Toast.makeText(requireContext(), "Permission are denied forever", Toast.LENGTH_SHORT).show()
+        } else {
+            AlertDialog.Builder(requireContext())
+                .setTitle("Разрешение отклонено")
+                .setMessage("""
+                    Вы отколнили разрешение навсегда.
+                    Вы можете изменить свое решение в настройках приложения.
+                    Перейти в настройки?
+                """.trimIndent())
+                .setPositiveButton("Открыть") { _, _ ->
+                    startActivity(appSettingIntent)
+                }
+                .create()
+                .show()
+        }
+    }
+
+    companion object{
+        const val COERCE_LOCATION_REQUEST_CODE = 999
+    }
 }

3c031f1b1ee9039c5f385b585c4f51dec0dca804
Andrey Onopriychuk <andromeda2516@gmail.com>
2022-11-16 02:50:20 +0700

    fix: add activity result api for geo permission

--- app/src/main/java/com/onopry/weatherapp_avito/presentation/screens/HomeFragment.kt
+++ app/src/main/java/com/onopry/weatherapp_avito/presentation/screens/HomeFragment.kt
@@ -10,8 +10,8 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -34,39 +34,64 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
     private val viewModel: HomeViewModel by viewModels()
 
+    private val geoLocationPermissionLauncher =
+        registerForActivityResult(RequestPermission(), ::onGrantedGeoPermissionResult)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
 
-        //        binding.textview.text = "asdasasdasd"
-        //        binding.temperatureIndicatorTv.text = "\uf02e"
-        checkAppPermissions()
+        geoLocationPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
 
+        observeForecastState()
+    }
+
+    private fun observeForecastState() {
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.forecastStateFlow.collect { forecastState: ForecastState ->
                     when (forecastState) {
                         is ForecastState.Success -> {
-                            binding.weatherStateImage.setImageByWeatherCode(
-                                forecastState.data.currentWeather.weatherCode
-                            )
-                            binding.tempIndicatorTv.text =
-                                forecastState.data.currentWeather.temperature.toString()
-
-                            binding.windDirectionsVal.text =
-                                forecastState.data.currentWeather.windDirection.toString()
-
-                            binding.windSpeedVal.text =
-                                forecastState.data.currentWeather.windSpeed.toString()
+                            bindCurrForecastData(forecastState)
                         }
+                        ForecastState.Empty -> TODO()
+                        is ForecastState.Error -> TODO()
+                        ForecastState.Loading -> TODO()
                     }
                 }
             }
         }
     }
 
+    private fun bindCurrForecastData(forecastState: ForecastState.Success) {
+        with(binding) {
+            weatherStateImage
+                .setImageByWeatherCode(forecastState.data.currentWeather.weatherCode)
+
+            tempIndicatorTv.text =
+                forecastState.data.currentWeather.temperature.toString()
+
+            windDirectionsVal.text =
+                forecastState.data.currentWeather.windDirection.toString()
+
+            windSpeedVal.text =
+                forecastState.data.currentWeather.windSpeed.toString()
+        }
+    }
+
+    private fun onGrantedGeoPermissionResult(granted: Boolean) {
+        if (granted) {
+            shortToast("Разрешенгие получено")
+            onLocationPermissionGranted()
+        } else {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                askUserForOpeningAppSettings()
+            }
+        }
+    }
+
     @SuppressLint("MissingPermission")
-    private fun getSystemLocation() {
+    private fun onLocationPermissionGranted() {
         val locationManager = LocationServices.getFusedLocationProviderClient(requireContext())
         viewModel.sendLocationState(LocationState.Pending)
         locationManager.lastLocation.addOnSuccessListener { location: Location? ->
@@ -81,42 +106,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             }
             viewModel.fetchForecast()
 
-        }
-    }
-
-    // Better to use Result API, but now it is what it is.
-    // todo: Waiting for improvement #1 - Result API for permissions
-    private fun checkAppPermissions() {
-        if (checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            //todo добавить пасхалку если успею
-            getSystemLocation()
-        } else {
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                COERCE_LOCATION_REQUEST_CODE
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == COERCE_LOCATION_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                shortToast("Разрешенгие получено")
-                getSystemLocation()
-            } else {
-                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                    askUserForOpeningAppSettings()
-                }
-            }
         }
     }
 
